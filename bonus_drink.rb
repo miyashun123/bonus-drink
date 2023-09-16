3dbaadbd25e962d1c15dba44e211537d7ea2a728
miyashun123 <144928265+miyashun123@users.noreply.github.com>
2023-09-16 10:36:36 +0000

    add

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,18 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    count = 0
+    new_bottle = 0
+    remainder = 0
+    used_bottle = amount
+    while used_bottle >= 3 do
+      new_bottle = used_bottle / 3
+      remainder = used_bottle % 3
+      used_bottle = new_bottle + remainder
+      count += new_bottle
+    end
+    return amount + count
   end
+  
 end
+amount = gets.chomp.to_i
+puts BonusDrink.total_count_for(amount)
